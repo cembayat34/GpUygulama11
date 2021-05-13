14a23f0d0c1378dd834b17de17ff99f7ddac698d
cem bayat <cembayat34@outlook.com>
2021-05-13 07:51:06 +0300

    Proje dosyası ekle.

---
+++ GpUygulama11/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace GpUygulama11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxBolunen = new System.Windows.Forms.TextBox();
+            this.textBoxBolen = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxBolum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxBolunen
+            // 
+            this.textBoxBolunen.Location = new System.Drawing.Point(18, 44);
+            this.textBoxBolunen.Name = "textBoxBolunen";
+            this.textBoxBolunen.Size = new System.Drawing.Size(72, 20);
+            this.textBoxBolunen.TabIndex = 1;
+            // 
+            // textBoxBolen
+            // 
+            this.textBoxBolen.Location = new System.Drawing.Point(107, 44);
+            this.textBoxBolen.Name = "textBoxBolen";
+            this.textBoxBolen.Size = new System.Drawing.Size(72, 20);
+            this.textBoxBolen.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(96, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(101, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(100, 5);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bolunen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bolen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bolum";
+            // 
+            // textBoxBolum
+            // 
+            this.textBoxBolum.Location = new System.Drawing.Point(108, 95);
+            this.textBoxBolum.Name = "textBoxBolum";
+            this.textBoxBolum.Size = new System.Drawing.Size(71, 20);
+            this.textBoxBolum.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 234);
+            this.Controls.Add(this.textBoxBolum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBoxBolen);
+            this.Controls.Add(this.textBoxBolunen);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxBolunen;
+        private System.Windows.Forms.TextBox textBoxBolen;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxBolum;
+    }
+}
+
---
+++ GpUygulama11/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GpUygulama11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBoxBolum.Text = (Convert.ToInt32(textBoxBolunen.Text) / Convert.ToInt32(textBoxBolen.Text)).ToString();
+            }
+            catch (DivideByZeroException)
+            {
+
+                MessageBox.Show("Bir sayi 0'a bolunemez!");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lutfen Sayi Giriniz!");
+
+            }
+            
+        }
+    }
+}
